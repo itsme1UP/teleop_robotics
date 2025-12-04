e080036f9fc664753d0bd8ce7e7834a153f4861a
itsme1UP <52211624+itsme1UP@users.noreply.github.com>
2025-12-04 09:12:06 +0500

    Update haptic_node.py

--- teleop/haptic_node.py
+++ teleop/haptic_node.py
@@ -5,23 +5,27 @@ class HapticNode:
     def __init__(self):
         rospy.init_node('haptic_node', anonymous=True)
 
-        # Placeholder subscriber, e.g., from velocity or impedance output
-        rospy.Subscriber('/ur10e/cmd_vel', Float32, self.callback)
+        # Subscribe to contact or force sensor topic from UR10e
+        rospy.Subscriber('/ur10e/contact_force', Float32, self.force_callback)
 
-        # Publisher for haptic feedback (placeholder)
+        # Publisher for haptic feedback intensity
         self.pub = rospy.Publisher('/haptic/feedback', Float32, queue_size=10)
 
-        rospy.loginfo('Haptic node started')
-        self.run()
-
-    def callback(self, msg):
-        # For now just pass velocity magnitude as haptic intensity
-        intensity = abs(msg.data)
-        self.pub.publish(Float32(data=intensity))
-
-    def run(self):
+        rospy.loginfo("Haptic node started")
         rospy.spin()
 
+    def force_callback(self, msg):
+        # Simple mapping from force to haptic intensity
+        intensity = msg.data * 0.8  # scale down to safe haptic range
+
+        # Clamp intensity between 0 and 1
+        if intensity > 1.0:
+            intensity = 1.0
+        elif intensity < 0.0:
+            intensity = 0.0
+
+        # Publish placeholder haptic feedback
+        self.pub.publish(Float32(data=intensity))
 
 if __name__ == '__main__':
     try:
